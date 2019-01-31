fa0ad7e4858d61e63fc8cddf27a53251d80f4c5d
Thomas Scholtes <thomas@monadic.xyz>
2019-01-31 16:17:25 +0100

    Add dummy bootstrap node to hide errors
    If no bootstrap node is provided the nodes will repeatedly log an error
    message about that. Having a dummy bootstrap node that is not reachable
    prevents logging these errors.

--- start-node.sh
+++ start-node.sh
@@ -11,6 +11,8 @@ if [ ! -e "$IPFS_PATH/config" ]; then
   ipfs init
   cp /data/swarm.key "$IPFS_PATH"
   ipfs bootstrap rm all >/dev/null
+  # Dummy boostrap node to prevent error messages
+  ipfs bootstrap add /ip4/127.0.0.1/tcp/1/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ
   ipfs config Addresses.API "/ip4/0.0.0.0/tcp/500${node_id}"
   ipfs config Addresses.Gateway "/ip4/0.0.0.0/tcp/808${node_id}"
   ipfs config Addresses.Swarm --json "[\"/ip4/127.0.0.1/tcp/400${node_id}\"]"
